2a1426121785089523b5a8cc02869dd3d213043e
AnnetteSuazo <79999643+AnnetteSuazo@users.noreply.github.com>
2021-05-21 13:07:41 -0600

    Add files via upload

---
+++ db_restaurante.sql
@@ -0,0 +1,233 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-05-2021 a las 20:53:53
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_restaurante`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_direcciones`
+--
+
+CREATE TABLE `tbl_direcciones` (
+  `ID_DIRECCION` int(11) NOT NULL,
+  `DIRECCION` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tbl_direcciones`
+--
+
+INSERT INTO `tbl_direcciones` (`ID_DIRECCION`, `DIRECCION`) VALUES
+(1, 'Colonia Lomas del rios'),
+(3, 'Barrio Arriba'),
+(4, 'Barrio san Blas'),
+(5, 'Barrio Lourdes'),
+(6, 'Barrio cruz');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_envios`
+--
+
+CREATE TABLE `tbl_envios` (
+  `ID_ENVIO` int(11) NOT NULL,
+  `DESCRIPCION_ENVIO` varchar(300) NOT NULL,
+  `FECHA` date NOT NULL,
+  `ID_DIRECCION` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tbl_envios`
+--
+
+INSERT INTO `tbl_envios` (`ID_ENVIO`, `DESCRIPCION_ENVIO`, `FECHA`, `ID_DIRECCION`) VALUES
+(1, 'vaso en sopa ', '2021-05-06', 5),
+(2, 'plato de arroz chino', '2021-05-06', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_facturas`
+--
+
+CREATE TABLE `tbl_facturas` (
+  `ID_FACTURA` int(11) NOT NULL,
+  `DESCRIPCION_FACTURA` varchar(200) NOT NULL,
+  `FECHA` date NOT NULL,
+  `ID_DIRECCIONES` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tbl_facturas`
+--
+
+INSERT INTO `tbl_facturas` (`ID_FACTURA`, `DESCRIPCION_FACTURA`, `FECHA`, `ID_DIRECCIONES`) VALUES
+(1, '2 vasos de sopa de pollo', '2021-05-06', 4),
+(2, '3 arroz chino familiar', '2021-05-13', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_inventario`
+--
+
+CREATE TABLE `tbl_inventario` (
+  `ID_PRODUCTO` int(11) NOT NULL,
+  `PRODUCTO` varchar(100) NOT NULL,
+  `PRECIO` double NOT NULL,
+  `ID_DIRECCION` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tbl_inventario`
+--
+
+INSERT INTO `tbl_inventario` (`ID_PRODUCTO`, `PRODUCTO`, `PRECIO`, `ID_DIRECCION`) VALUES
+(1, 'coca portátil 2l.', 58, 1),
+(2, 'sopa instantánea', 12, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_pedidos`
+--
+
+CREATE TABLE `tbl_pedidos` (
+  `ID_PEDIDO` int(11) NOT NULL,
+  `DESCRIPCION_PEDIDO` varchar(200) NOT NULL,
+  `FECHA` date NOT NULL,
+  `ID_DIRECCION` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tbl_pedidos`
+--
+
+INSERT INTO `tbl_pedidos` (`ID_PEDIDO`, `DESCRIPCION_PEDIDO`, `FECHA`, `ID_DIRECCION`) VALUES
+(1, '5 vasos de sopa instantánea', '2021-05-13', 1),
+(2, '3 cajas de pizza', '2021-05-19', 6);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_direcciones`
+--
+ALTER TABLE `tbl_direcciones`
+  ADD PRIMARY KEY (`ID_DIRECCION`);
+
+--
+-- Indices de la tabla `tbl_envios`
+--
+ALTER TABLE `tbl_envios`
+  ADD PRIMARY KEY (`ID_ENVIO`),
+  ADD KEY `FK_DIRECIONES_ENVIOS` (`ID_DIRECCION`);
+
+--
+-- Indices de la tabla `tbl_facturas`
+--
+ALTER TABLE `tbl_facturas`
+  ADD PRIMARY KEY (`ID_FACTURA`),
+  ADD KEY `FK_DIRECCIONES_FACTURAS` (`ID_DIRECCIONES`);
+
+--
+-- Indices de la tabla `tbl_inventario`
+--
+ALTER TABLE `tbl_inventario`
+  ADD PRIMARY KEY (`ID_PRODUCTO`),
+  ADD KEY `FK_DIRECCION_INVENTARIO` (`ID_DIRECCION`);
+
+--
+-- Indices de la tabla `tbl_pedidos`
+--
+ALTER TABLE `tbl_pedidos`
+  ADD PRIMARY KEY (`ID_PEDIDO`),
+  ADD KEY `FK_DIRECCIONES_PEDIDOS` (`ID_DIRECCION`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_direcciones`
+--
+ALTER TABLE `tbl_direcciones`
+  MODIFY `ID_DIRECCION` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_envios`
+--
+ALTER TABLE `tbl_envios`
+  MODIFY `ID_ENVIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_facturas`
+--
+ALTER TABLE `tbl_facturas`
+  MODIFY `ID_FACTURA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_inventario`
+--
+ALTER TABLE `tbl_inventario`
+  MODIFY `ID_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_pedidos`
+--
+ALTER TABLE `tbl_pedidos`
+  MODIFY `ID_PEDIDO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_envios`
+--
+ALTER TABLE `tbl_envios`
+  ADD CONSTRAINT `FK_DIRECCIONES_ENVIOS` FOREIGN KEY (`ID_DIRECCION`) REFERENCES `tbl_direcciones` (`ID_DIRECCION`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tbl_facturas`
+--
+ALTER TABLE `tbl_facturas`
+  ADD CONSTRAINT `FK_DIRECCIONES_FACTURAS` FOREIGN KEY (`ID_DIRECCIONES`) REFERENCES `tbl_direcciones` (`ID_DIRECCION`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `tbl_inventario`
+--
+ALTER TABLE `tbl_inventario`
+  ADD CONSTRAINT `FK_DIRECCIONES_INVENTARIO` FOREIGN KEY (`ID_DIRECCION`) REFERENCES `tbl_direcciones` (`ID_DIRECCION`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tbl_pedidos`
+--
+ALTER TABLE `tbl_pedidos`
+  ADD CONSTRAINT `FK_DIRECCIONES_PEDIDOS` FOREIGN KEY (`ID_DIRECCION`) REFERENCES `tbl_direcciones` (`ID_DIRECCION`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
